46e1082b9816f2edd0592af543cfa374ef4f44f7
Maor <maorsa9@gmail.com>
2023-10-31 13:59:30 +0200

    added a jwt token to signup a student

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,26 @@
+const jwt = require("jsonwebtoken");
 const Student = require("./../models/studentModel");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newStudent = await Student.create(req.body);
+  const newStudent = await Student.create({
+    name: req.body.name,
+    age: req.body.age,
+    academic: req.body.academic,
+    department: req.body.department,
+    yearbook: req.body.yearbook,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newStudent._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       student: newStudent,
     },
--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const studentSchema = new mongoose.Schema({
   name: {
@@ -37,8 +38,27 @@ const studentSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, "סיסמאות לא תואמות"],
+    required: [true, "נא למלא אישור סיסמה"],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "!סיסמאות לא תואמות",
+    },
   },
+});
+
+studentSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const Student = mongoose.model("Student", studentSchema);
